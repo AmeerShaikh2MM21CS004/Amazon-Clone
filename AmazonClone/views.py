ace4f9addb7d16036d2230209f781540eaac9320
Ameer Shaikh <amir140302@gmail.com>
2024-10-11 12:22:59 +0530

    dynamic username added using POST method

--- AmazonClone/views.py
+++ AmazonClone/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
 
 def home(request):
-    return render(request,"index.html")
+    if request.method=="GET":
+        username=request.GET.get("user")
+    return render(request,"index.html",{"username":username})
 
 def sign_in(request):
+    if request.method=="POST":
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+        url = "/?user={}".format(username)
+        return HttpResponseRedirect(url)
     return render(request,"signin.html")
 
 def return_ord(request):
@@ -11,3 +20,4 @@ def return_ord(request):
 
 def cart(request):
     return render(request,"cart.html")
+
